3415da1f31d3369b595c64430f0864d737ac6f73
Erik Golinelli <erik@codekraft.it>
2024-04-21 13:34:01 +0200

    (wip) validators

---
+++ src/validators.ts
@@ -0,0 +1,13 @@
+const validators: Record<string, RegExp> = {
+	hex: /^#([\da-f]{3,8})/i,
+	rgb: /^rgba?\(([^)]+)\)/i,
+	hsl: /^hsla?\(([^)]+)\)/i,
+	lab: /^lab\(([^)]+)\)/i,
+	hwb: /^hwb\(([^)]+)\)/i,
+	lch: /^lch\(([^)]+)\)/i,
+	oklab: /^oklab\(([^)]+)\)/i,
+	oklch: /^oklch\(([^)]+)\)/i,
+	color: /^color\(([^)]+)\)/i,
+};
+
+export default validators;
