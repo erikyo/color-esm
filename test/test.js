7155d8f78497f805a743637087ce04cc5c170a6a
Erik Golinelli <erik@codekraft.it>
2026-03-14 19:27:49 +0100

    refactor(tests): remove unused labTest file and update imports in test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 import assert from "node:assert";
 import test from "node:test";
-import Color from "../lib/esm/index.js";
+import Color from "../lib/index.js";
 
 test("rgb", () => {
   const black = new Color();
